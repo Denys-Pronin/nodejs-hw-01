5c135f253021f166857f0742592639042d22ed10
Denys Pronin <denispronindev@gmail.com>
2024-09-01 17:30:41 +0300

    Finish

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,19 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    let contacts = [];
+    try {
+      const data = await fs.readFile(PATH_DB, 'utf-8');
+      contacts = JSON.parse(data);
+    } catch (error) {
+      contacts = [];
+      console.log('Помилка: ', error);
+    }
+    contacts = contacts.concat(updatedContacts);
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+  } catch (error) {
+    console.log('Помилка: ', error);
+  }
+};
